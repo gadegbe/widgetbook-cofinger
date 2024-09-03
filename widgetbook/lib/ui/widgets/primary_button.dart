de86bc61182f2fa70327230343e4699cf7fada2d
Patrice GADEGBE <patricegadegbe@gmail.com>
2024-09-03 16:44:03 +0200

    chore: Update primary button widget to include ellipsis for long content

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -4,6 +4,18 @@ import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:groceries_app/ui/ui.dart';
 import 'package:widgetbook/widgetbook.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
-/// Add Widgetbook use-case here
+@widgetbook.UseCase(
+  name: 'Default',
+  type: PrimaryButton,
+)
+Widget buildPrimaryButtonUseCase(BuildContext context) {
+  return PrimaryButton(
+    onPressed: () {},
+    content: context.knobs.string(
+      label: 'Content',
+      initialValue: 'Awesome primary button',
+    ),
+  );
+}
